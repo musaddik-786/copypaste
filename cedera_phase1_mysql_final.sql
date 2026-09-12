fd7e9cecfa5695de19078ce591e9aa75792b5fff
musaddik-786 <85271806+musaddik-786@users.noreply.github.com>
2026-09-12 15:51:34 +0530

    Update cedera_phase1_mysql_final.sql

--- cedera_phase1_mysql_final.sql
+++ cedera_phase1_mysql_final.sql
@@ -1,8 +1,1 @@
-(venv) JarvisClaims@JarvisClaims:~/Musaddique/cedera/MCP$ ip route
-default via 10.0.0.1 dev eth0 proto dhcp src 10.0.0.4 metric 100 
-10.0.0.0/24 dev eth0 proto kernel scope link src 10.0.0.4 metric 100 
-10.0.0.1 dev eth0 proto dhcp scope link src 10.0.0.4 metric 100 
-168.63.129.16 via 10.0.0.1 dev eth0 proto dhcp src 10.0.0.4 metric 100 
-169.254.169.254 via 10.0.0.1 dev eth0 proto dhcp src 10.0.0.4 metric 100 
-172.17.0.0/16 dev docker0 proto kernel scope link src 172.17.0.1 linkdown 
-(venv) JarvisClaims@JarvisClaims:~/Musaddique/cedera/MCP$ 
+python3 -c "import socket; s=socket.socket(); s.settimeout(3); print(s.connect_ex(('10.0.0.1',3306)))"
