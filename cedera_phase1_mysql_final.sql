7ea90757813b68bf61a90763ca9f06e105b36d2e
musaddik-786 <85271806+musaddik-786@users.noreply.github.com>
2026-09-12 15:54:10 +0530

    Update cedera_phase1_mysql_final.sql

--- cedera_phase1_mysql_final.sql
+++ cedera_phase1_mysql_final.sql
@@ -1,3 +1,7 @@
-ping -c 2 LTCH-9DT24253GP
-
-python3 -c "import socket; print(socket.gethostbyname('LTCH-9DT24253GP'))"
+(venv) JarvisClaims@JarvisClaims:~/Musaddique/cedera/MCP$ ping -c 2 LTCH-9DT24253GP
+ping: LTCH-9DT24253GP: Name or service not known
+(venv) JarvisClaims@JarvisClaims:~/Musaddique/cedera/MCP$ python3 -c "import socket; print(socket.gethostbyname('LTCH-9DT24253GP'))"
+Traceback (most recent call last):
+  File "<string>", line 1, in <module>
+socket.gaierror: [Errno -2] Name or service not known
+(venv) JarvisClaims@JarvisClaims:~/Musaddique/cedera/MCP$ 
