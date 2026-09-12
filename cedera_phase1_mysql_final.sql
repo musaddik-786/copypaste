41ac9aa0e68c6148104dfafce247a8e2fc2c5548
musaddik-786 <85271806+musaddik-786@users.noreply.github.com>
2026-09-12 15:56:35 +0530

    Update cedera_phase1_mysql_final.sql

--- cedera_phase1_mysql_final.sql
+++ cedera_phase1_mysql_final.sql
@@ -1,1 +1,1 @@
-python3 -c "import socket; s=socket.socket(); s.settimeout(3); print(s.connect_ex(('WINDOWS_IP',3306)))"
+python3 -c "import socket; s=socket.socket(); s.settimeout(5); s.connect(('10.0.0.1',3306)); print('CONNECTED')"
