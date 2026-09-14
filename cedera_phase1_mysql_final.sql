b6c2851a982ec9ab4d8fa9aeb0cdda7b196b8ea1
musaddik-786 <85271806+musaddik-786@users.noreply.github.com>
2026-09-14 10:36:34 +0530

    Update cedera_phase1_mysql_final.sql

--- cedera_phase1_mysql_final.sql
+++ cedera_phase1_mysql_final.sql
@@ -1,6 +1,11 @@
-JarvisClaims@JarvisClaims:~/Musaddique$ timeout 5 bash -c '</dev/tcp/10.6.2.27/3360' && echo "CONNECTED" || echo "FAILED"
-FAILED
-JarvisClaims@JarvisClaims:~/Musaddique$ ip route get 10.6.2.27
-10.6.2.27 via 10.0.0.1 dev eth0 src 10.0.0.4 uid 1000 
-    cache 
-JarvisClaims@JarvisClaims:~/Musaddique$ 
+C:\Users\2000137378>ipconfig
+
+Windows IP Configuration
+
+
+Ethernet adapter Ethernet 4:
+
+   Connection-specific DNS Suffix  . :
+   IPv4 Address. . . . . . . . . . . : 10.6.2.27
+   Subnet Mask . . . . . . . . . . . : 255.255.255.255
+   Default Gateway . . . . . . . . . : 0.0.0.0
