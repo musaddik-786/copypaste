5659820b274fd9ac2fffb072e58f56e5b706dc9d
musaddik-786 <85271806+musaddik-786@users.noreply.github.com>
2026-09-14 10:35:00 +0530

    Revise connectivity commands in SQL file
    Updated connectivity testing commands for TCP/MySQL.

--- cedera_phase1_mysql_final.sql
+++ cedera_phase1_mysql_final.sql
@@ -1,19 +1,9 @@
-JarvisClaims@JarvisClaims:~/Musaddique$ ping -c 4 10.0.0.1
-PING 10.0.0.1 (10.0.0.1) 56(84) bytes of data.
+Linux has Bash's built-in /dev/tcp, so run:
 
---- 10.0.0.1 ping statistics ---
-4 packets transmitted, 0 received, 100% packet loss, time 3052ms
+timeout 5 bash -c '</dev/tcp/10.6.2.27/3360' && echo "CONNECTED" || echo "FAILED"
 
-JarvisClaims@JarvisClaims:~/Musaddique$ ping -c 4 8.8.8.8
-PING 8.8.8.8 (8.8.8.8) 56(84) bytes of data.
-64 bytes from 8.8.8.8: icmp_seq=1 ttl=115 time=5.17 ms
-64 bytes from 8.8.8.8: icmp_seq=2 ttl=115 time=5.29 ms
-64 bytes from 8.8.8.8: icmp_seq=3 ttl=115 time=5.46 ms
-64 bytes from 8.8.8.8: icmp_seq=4 ttl=115 time=5.67 ms
+This is much more useful than ping because your actual requirement is TCP/MySQL connectivity, not ICMP.
 
---- 8.8.8.8 ping statistics ---
-4 packets transmitted, 4 received, 0% packet loss, time 3004ms
-rtt min/avg/max/mdev = 5.171/5.398/5.674/0.189 ms
-JarvisClaims@JarvisClaims:~/Musaddique$ nc -vz 10.6.2.27 3360
-bash: nc: command not found
-JarvisClaims@JarvisClaims:~/Musaddique$ 
+Then also run:
+
+ip route get 10.6.2.27
