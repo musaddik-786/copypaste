bd9c09f9b48d2506b3374ac12951252e330151a4
musaddik-786 <85271806+musaddik-786@users.noreply.github.com>
2026-09-16 18:19:50 +0530

    Update dummy_test_data.sql

--- dummy_test_data.sql
+++ dummy_test_data.sql
@@ -1,5 +1,3 @@
-SELECT COUNT(*) FROM motor_fnol_mandatory_fields;
-SELECT COUNT(*) FROM motor_policy_details;
-SELECT COUNT(*) FROM motor_vehicle_details;
-SELECT COUNT(*) FROM policy_details WHERE policy_number IN ('73-300676','73-400676','73-123676');
-SELECT COUNT(*) FROM fnol_mandatory_fields;
+SELECT column_name FROM information_schema.columns WHERE table_name = 'motor_policy_details' ORDER BY ordinal_position;
+SELECT column_name FROM information_schema.columns WHERE table_name = 'motor_vehicle_details' ORDER BY ordinal_position;
+SELECT column_name FROM information_schema.columns WHERE table_name = 'motor_fnol_mandatory_fields' ORDER BY ordinal_position;
